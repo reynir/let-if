7d508d2e760be1720cb827cfea6be0261128421a
Reynir Björnsson <reynir@reynir.dk>
2022-03-11 10:40:14 +0000

    Refactor, update CHANGES.md

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,9 @@
+# v0.3.0
+
+* Add new if%true that is a if expression that returns the value of the condition.
+
+* Refactor let%if. Error locations are slightly different.
+
 # v0.2.0
 
 * Remove references to jbuilder and switch to dune completely
--- src/let_if.ml
+++ src/let_if.ml
@@ -1,33 +1,24 @@
 open Ppxlib
 open Ast_builder.Default
 
-let expand_let ~let_loc:loc
-    rec_flag
-    (bindings : Ppxlib.value_binding list)
-    (expression : Ppxlib.expression) 
-  : Ppxlib.expression =
-  match rec_flag with 
-  | Recursive ->
-    Location.raise_errorf ~loc "[%%if ] cannot apply to a recursive let binding"
-  | Nonrecursive ->
+let expand_let ~ctxt:_ rec_flag bindings expression =
+  match rec_flag with
+  | { txt = Recursive; loc } ->
+    Location.raise_errorf ~loc "[%%if ] cannot apply to a recursive let-binding"
+  | { txt = Nonrecursive; loc = _ } ->
     match bindings with
-    | [{ pvb_pat; pvb_expr; pvb_attributes = _; pvb_loc = _ }] ->
+    | [{ pvb_pat; pvb_expr; pvb_attributes = _; pvb_loc = loc }] ->
       [%expr match [%e pvb_expr] with
           | [%p pvb_pat] -> [%e expression]
           | _ -> () ]
-    | _ ->
+    | _ :: { pvb_loc = loc; _ } :: _ ->
       Location.raise_errorf ~loc "[%%if ] cannot apply to let-and bindings"
-
-let expand_let ~loc ~path:_ e =
-  Ast_pattern.parse
-    Ast_pattern.(pexp_let __ __ __) loc e ~on_error:(fun () ->
-        Location.raise_errorf ~loc "[%%if ] must apply to a let statement")
-    (expand_let ~let_loc:e.pexp_loc)
+    | [] -> assert false
 
 let let_if : Extension.t =
-  Extension.declare "if"
+  Extension.V3.declare "if"
     Extension.Context.Expression
-    Ast_pattern.(single_expr_payload __)
+    Ast_pattern.(single_expr_payload (pexp_let __' __ __))
     expand_let
 
 let expand_if ~ctxt:_ if_ then_ else_ =
@@ -35,9 +26,13 @@ let expand_if ~ctxt:_ if_ then_ else_ =
   let loc = if_.pexp_loc in
   match else_ with
   | Some else_ ->
-    [%expr let [%p pvar ~loc c] = [%e if_] in (if [%e evar ~loc c] then [%e then_] else [%e else_]); [%e evar ~loc c] ]
+    [%expr let [%p pvar ~loc c] = [%e if_] in
+      (if [%e evar ~loc c] then [%e then_] else [%e else_]);
+      [%e evar ~loc c] ]
   | None ->
-    [%expr let [%p pvar ~loc c] = [%e if_] in (if [%e evar ~loc c] then [%e then_]); [%e evar ~loc c]]
+    [%expr let [%p pvar ~loc c] = [%e if_] in
+      (if [%e evar ~loc c] then [%e then_]);
+      [%e evar ~loc c] ]
 
 let if_true : Extension.t =
   Extension.V3.declare "true"
